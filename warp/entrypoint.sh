6530f6ba0f98a697ba1d26ac03a04147fc131ecc
xiaocao <wdjoys@hotmail.com>
2023-04-20 16:08:30 +0800

    a

---
+++ warp/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# @Author: xiaocao
+# @Date:   2023-04-20 15:44:28
+# @Last Modified by:   xiaocao
+# @Last Modified time: 2023-04-20 16:06:39
+warp-cli register && warp-cli set-mode proxy
