904e01f8fc95d62d896220aa1db930319e900ce0
xiaocao <wdjoys@hotmail.com>
2023-04-20 18:43:39 +0800

    1

--- warp/entrypoint.sh
+++ warp/entrypoint.sh
@@ -2,9 +2,16 @@
 # @Author: xiaocao
 # @Date:   2023-04-20 15:44:28
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-04-20 17:29:05
-warp-cli register &&
-    warp-cli connect &&
-    warp-cli set-mode proxy 40001 &&
-    warp-cli set-proxy-port &&
-    warp-cli enable-always-on
+# @Last Modified time: 2023-04-20 18:43:33
+(
+    while ! warp-cli --accept-tos register; do
+        sleep 1
+        echo >&2 "Awaiting warp-svc become online..."
+    done
+    warp-cli --accept-tos set-mode proxy
+    warp-cli --accept-tos set-proxy-port 40001
+    warp-cli --accept-tos connect
+    haproxy -f /etc/haproxy/haproxy.cfg
+) &
+
+exec warp-svc
