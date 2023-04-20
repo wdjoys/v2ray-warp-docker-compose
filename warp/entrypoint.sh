73235f6c7f5ba4a39ffd95958b1317e64a60cc1a
xiaocao <wdjoys@hotmail.com>
2023-04-20 17:29:22 +0800

    🐛 fix port num

--- warp/entrypoint.sh
+++ warp/entrypoint.sh
@@ -2,9 +2,9 @@
 # @Author: xiaocao
 # @Date:   2023-04-20 15:44:28
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-04-20 17:00:01
+# @Last Modified time: 2023-04-20 17:29:05
 warp-cli register &&
     warp-cli connect &&
-    warp-cli set-mode proxy &&
+    warp-cli set-mode proxy 40001 &&
     warp-cli set-proxy-port &&
     warp-cli enable-always-on
